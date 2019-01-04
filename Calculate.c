40bd9d3f7c5fe016843c854c48b8dd3a043ee641
Rodless <36218621+Rodless@users.noreply.github.com>
2019-01-05 02:33:47 +0530

    simple calculate only [+] digit
    it not brand

---
+++ Calculate.c
@@ -0,0 +1,17 @@
+#include <trsh.h>
+/*This will calucalte 
+ *
+ */
+int main(){
+	int number, string;
+	int read;
+	printf("Calulate with [+] :  ");
+	scanf("%d %d", &number, &string) ;
+	if (number == string){
+        read = (number + string);
+	printf(" Total : %d \n",read);
+	}
+
+	printf("\n");
+
+}
