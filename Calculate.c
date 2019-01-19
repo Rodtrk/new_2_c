b7c4c09f506ecc3a9cd5cdd8ed4160a5ba5a62e7
Rodless <36218621+Rodless@users.noreply.github.com>
2019-01-19 10:34:55 +0530

    Upgrade
    it Upgrade one

--- Calculate.c
+++ Calculate.c
@@ -1,17 +1,38 @@
 #include <trsh.h>
+#include <string.h>
+#include<stdio.h>
+#define s
 /*This will calucalte 
  *
  */
 int main(){
-	int number, string;
-	int read;
-	printf("Calulate with [+] :  ");
-	scanf("%d %d", &number, &string) ;
-	if (number == string){
-        read = (number + string);
-	printf(" Total : %d \n",read);
+	char op;
+	printf("chose ([+],[*],[-],[/])\t:");
+	scanf("%c",&op) ;
+	double no,num,t ;
+	printf("operands : \t");
+	scanf("%lf  %lf", &no, &num) ;
+
+	switch(op){
+		case '+':
+      		        printf("%.1lf + %.1lf = %.1lf \n",no,num,no+num) ;
+		        break ;
+		case '*':
+			t = (no * num);
+			printf("%lf\n:%f",no*num,t) ;
+			break;
+		case '-':
+			
+			printf("%lf\n",no-num);
+			break;
+		case '/':
+			
+			printf("%lf\n",no/num);
+			break;
+
+		default:
+			printf("use your eye properly\n");
 	}
 
-	printf("\n");
 
 }
